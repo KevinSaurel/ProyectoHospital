9bfbfd247e3cfabe798aa13979b4725cfa1bf1f7
KevinSaurel <kevin.saurel@opendeusto.es>
2024-10-09 19:47:55 +0200

    Cambio de nombre attributo (porque parece que no se escribir)

--- src/clases/Doctor.java
+++ src/clases/Doctor.java
@@ -1,13 +1,29 @@
 package clases;
 
 public class Doctor extends Persona{
-	protected String especiabilidad;
+	protected String especialidad;
 	protected String horario;
 	
-	public Doctor(String contrasena, String nombre, String apellido, int edad, String ubicacion, String especiabilidad,
+	public Doctor(String contrasena, String nombre, String apellido, int edad, String ubicacion, String especialidad,
 			String horario) {
 		super(contrasena, nombre, apellido, edad, ubicacion);
-		this.especiabilidad = especiabilidad;
+		this.especialidad = especialidad;
+		this.horario = horario;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
 		this.horario = horario;
 	}
 	
