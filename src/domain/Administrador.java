21f81f5b21aefe192d24d76010ac5b1f44f1814e
mikel Oyarzabal <mikel.oyarzabal@opendeusto.es>
2024-12-09 18:32:36 +0100

    fallos arreglados, vamoooooooooooo

--- src/domain/Administrador.java
+++ src/domain/Administrador.java
@@ -2,20 +2,20 @@ package domain;
 
 public class Administrador extends Persona{
 
-protected String turno;
+	protected String turno;
 
-public Administrador(String contrasena, String nombre, String apellido, int edad, String ubicacion, String turno) {
-	super(contrasena, nombre, apellido, edad, ubicacion);
-	this.turno = turno;
-}
+	public Administrador(String contrasena, String nombre, String apellido, int edad, String ubicacion, String turno) {
+		super(contrasena, nombre, apellido, edad, ubicacion);
+		this.turno = turno;
+	}
 
-public String getTurno() {
-	return turno;
-}
-
-public void setTurno(String turno) {
-	this.turno = turno;
-}
+	public String getTurno() {
+		return turno;
+	}
+	
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
 
 
 }
